d2ac6c38011a07b27fcf38eff0b600475567435c
Bowie <leandro.h1512@gmail.com>
2024-07-07 09:24:46 -0300

    Criado a classe de marca produto e criado a tabela no banco de dados.

---
+++ src/main/java/br/com/loja_virtual/model/MarcaProduto.java
@@ -0,0 +1,36 @@
+package br.com.loja_virtual.model;
+
+import java.io.Serializable;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "marca_produto") // Indicar o nome da tabela.
+@SequenceGenerator(name = "seq_marca_produto", sequenceName = "seq_marca_produto", allocationSize = 1, initialValue = 1) // Define uma sequencia nas alterações das tabelas.
+public class MarcaProduto implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_marca_produto") // Define a estrategia do SequenceGenerator
+	private Long id;
+	private String nomeDesc;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNomeDesc() {
+		return nomeDesc;
+	}
+	public void setNomeDesc(String nomeDesc) {
+		this.nomeDesc = nomeDesc;
+	}
+
+}
